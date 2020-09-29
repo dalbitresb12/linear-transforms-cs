93ffef7c8fd0dc366e822d4ff1197b30fc72dd8f
Diego Albitres <diego.albitres@gmail.com>
2020-09-29 18:36:28 -0500

    Created new form for drawing board

---
+++ WinFormsApp/DrawingActivity.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp {
+    partial class drawingActivity {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.drawingBoard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawingBoard
+            // 
+            this.drawingBoard.BackColor = System.Drawing.Color.White;
+            this.drawingBoard.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.drawingBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingBoard.Location = new System.Drawing.Point(3, 3);
+            this.drawingBoard.Name = "drawingBoard";
+            this.drawingBoard.Size = new System.Drawing.Size(794, 444);
+            this.drawingBoard.TabIndex = 0;
+            this.drawingBoard.TabStop = false;
+            // 
+            // drawingActivity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.drawingBoard);
+            this.Name = "drawingActivity";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.Text = "Lienzo de Dibujo";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingBoard;
+    }
+}
---
+++ WinFormsApp/DrawingActivity.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsApp {
+    public partial class drawingActivity : Form {
+        public drawingActivity() {
+            InitializeComponent();
+        }
+    }
+}
